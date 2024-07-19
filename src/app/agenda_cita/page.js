9bc2c3a973a5bedc99a5cf61eb7d17f9cea25c66
AlbertoOro <bet68guti@gmail.com>
2024-07-19 01:53:16 -0600

    Correcion de suspense

--- src/app/agenda_cita/page.js
+++ src/app/agenda_cita/page.js
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { Formulario } from "../ui/AgendaCita/Formulario";
 
 export default function AgendarCita() {
   return (
     <main className="px-2 py-20 sm:p-24">
-      <Formulario />
+      <Suspense>
+        <Formulario />
+      </Suspense>
     </main>
   );
 }
